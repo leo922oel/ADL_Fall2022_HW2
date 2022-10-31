db99a079a244693aa7b61249f2bb24a228bd0b8f
leo922oel <leo922oel@gmail.com>
2022-10-31 15:35:45 +0800

    need to impl. main()

--- question-answering/run_qa.py
+++ question-answering/run_qa.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
-# Copyright The HuggingFace Team and The HuggingFace Inc. team. All rights reserved.
+# Copyright 2020 The HuggingFace Team All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,37 +14,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Fine-tuning the library models for multiple choice.
+Fine-tuning the library models for question answering using a slightly adapted version of the 🤗 Trainer.
 """
-# You can also adapt this script on your own multiple choice task. Pointers for this are left as comments.
+# You can also adapt this script on your own question answering task. Pointers for this are left as comments.
 
 import logging
+from optparse import Option
 import os
 import sys
-import json
 from dataclasses import dataclass, field
-from itertools import chain
-from typing import Optional, Union
+from typing import Optional
 
-import numpy as np
-import random
-import torch
-from tqdm import tqdm
+import datasets
 from datasets import load_dataset
 
+import evaluate
+import transformers
+from trainer_qa import QuestionAnsweringTrainer
 from transformers import (
     AutoConfig,
-    AutoModelForMultipleChoice,
+    AutoModelForQuestionAnswering,
     AutoTokenizer,
+    DataCollatorWithPadding,
+    EvalPrediction,
     HfArgumentParser,
-    Trainer,
+    PreTrainedTokenizerFast,
     TrainingArguments,
     default_data_collator,
     set_seed,
 )
-from transformers.tokenization_utils_base import PreTrainedTokenizerBase
-from transformers.trainer_utils import get_last_checkpoint, is_main_process
-from transformers.utils import PaddingStrategy, check_min_version, send_example_telemetry
+from transformers.trainer_utils import get_last_checkpoint
+from transformers.utils import check_min_version, send_example_telemetry
+from transformers.utils.versions import require_version
+from utils_qa import postprocess_qa_predictions
+
 
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
 check_min_version("4.22.2")
@@ -76,12 +79,6 @@ class ModelArguments:
             "help": "Path to a directory in which a downloaded pretrained model configuration should be cached if the standard cache should not be used"
             }
         )
-    use_fast_tokenizer: bool = field(
-        default=True, 
-        metadata={
-            "help": "Whether to use one of the fast tokenizer"
-        }
-    )
     revision: str = field(
         default='main', 
         metadata={
@@ -97,6 +94,18 @@ class ModelArguments:
 
 @dataclass
 class DataTrainingArguments:
+    dataset_name: Optional[str] = field(
+        default=None, 
+        metadata={
+            "help": "The name of the dataset to use"
+        }
+    )
+    dataset_config_name: Optional[str] = field(
+        default=None, 
+        metadata={
+            "help": "The config name of the dataset to use"
+        }
+    )
     train_file: Optional[str] = field(
         default=None, 
         metadata={
@@ -175,71 +184,58 @@ class DataTrainingArguments:
             "value if set.")
         }
     )
+    version_2_with_negative: bool = field(
+        default=False, 
+        metadata={
+            "help": "If true, some of the examples do not have an answer."
+        }
+    )
+    null_score_diff_threshold: float = field(
+        default=0.0,
+        metadata={
+            "help": ("The threshold used to select the null answer: if the best answer has a score that is less than "
+                "the score of the null answer minus this threshold, the null answer is selected for this example. "
+                "Only useful when `version_2_with_negative=True`.")
+        }
+    )
+    doc_stride: int = field(
+        default=128,
+        metadata={
+            "help": "When splitting up a long document into chunks, how much stride to take between chunks."
+        }
+    )
+    n_best_size: int = field(
+        default=20,
+        metadata={
+            "help": "The total number of n-best predictions to generate when looking for an answer."
+        }
+    )
+    max_answer_length: int = field(
+        default=30,
+        metadata={
+            "help": ("The maximum length of an answer that can be generated. This is needed because the start "
+                "and end predictions are not conditioned on one another.")
+        },
+    )
 
     def __post_init__(self):
-        if self.train_file is not None:
-            extension = self.train_file.split(".")[-1]
-            assert extension in ["csv", "json"], f"`train_file` should be a csv or a json file. (error: {extension})"
-        if self.valid_file is not None:
-            extension = self.valid_file.split(".")[-1]
-            assert extension in ["csv", "json"], f"`valid_file` should be a csv or a json file. (error: {extension})"
-        if self.test_file is not None:
-            extension = self.test_file.split(".")[-1]
-            assert extension in ["csv", "json"], f"`test_file` should be a csv or a json file. (error: {extension})"
-
-@dataclass
-class DataCollatorForMultipleChoice:
-    """
-    Data collator that will dynamically pad the inputs for multiple choice received.
-    Args:
-        tokenizer ([`PreTrainedTokenizer`] or [`PreTrainedTokenizerFast`]):
-            The tokenizer used for encoding the data.
-        padding (`bool`, `str` or [`~utils.PaddingStrategy`], *optional*, defaults to `True`):
-            Select a strategy to pad the returned sequences (according to the model's padding side and padding index)
-            among:
-            - `True` or `'longest'`: Pad to the longest sequence in the batch (or no padding if only a single sequence
-              if provided).
-            - `'max_length'`: Pad to a maximum length specified with the argument `max_length` or to the maximum
-              acceptable input length for the model if that argument is not provided.
-            - `False` or `'do_not_pad'` (default): No padding (i.e., can output a batch with sequences of different
-              lengths).
-        max_length (`int`, *optional*):
-            Maximum length of the returned list and optionally padding length (see above).
-        pad_to_multiple_of (`int`, *optional*):
-            If set will pad the sequence to a multiple of the provided value.
-            This is especially useful to enable the use of Tensor Cores on NVIDIA hardware with compute capability >=
-            7.5 (Volta).
-    """
-
-    tokenizer: PreTrainedTokenizerBase
-    padding: Union[bool, str, PaddingStrategy] = True
-    max_length: Optional[int] = None
-    pad_to_multiple_of: Optional[int] = None
-
-    def __call__(self, features):
-        label_name = "label" if "label" in features[0].keys() else "labels"
-        labels = [feature.pop(label_name) for feature in features]
-        batch_size = len(features)
-        num_choices = len(features[0]["input_ids"])
-        flattened_features = [
-            [{k: v[i] for k, v in feature.items()} for i in range(num_choices)] for feature in features
-        ]
-        flattened_features = list(chain(*flattened_features))
-        # flattened_features = sum(flattened_features, [])
-
-        batch = self.tokenizer.pad(
-            flattened_features,
-            padding=self.padding,
-            max_length=self.max_length,
-            pad_to_multiple_of=self.pad_to_multiple_of,
-            return_tensors="pt",
-        )
-
-        # Un-flatten
-        batch = {k: v.view(batch_size, num_choices, -1) for k, v in batch.items()}
-        # Add back labels
-        batch["labels"] = torch.tensor(labels, dtype=torch.int64)
-        return batch
+        if (
+            self.dataset_name is None and
+            self.train_file is None and
+            self.valid_file is None and
+            self.test_file is None
+        ):
+            raise ValueError("Need either a dataset name or a train/valid/test file")
+        else:
+            if self.train_file is not None:
+                extension = self.train_file.split(".")[-1]
+                assert extension in ["csv", "json"], f"`train_file` should be a csv or a json file. (error: {extension})"
+            if self.valid_file is not None:
+                extension = self.valid_file.split(".")[-1]
+                assert extension in ["csv", "json"], f"`valid_file` should be a csv or a json file. (error: {extension})"
+            if self.test_file is not None:
+                extension = self.test_file.split(".")[-1]
+                assert extension in ["csv", "json"], f"`test_file` should be a csv or a json file. (error: {extension})"
 
 def main():
     pass
